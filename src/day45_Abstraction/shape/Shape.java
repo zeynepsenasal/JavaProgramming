59e358f4a483d6a076bbce0406b8600b5ce13267
Zeynep Sena <zeynepssaltik@gmail.com>
2022-05-05 13:01:00 +0300

    dy45 Abstraction

---
+++ src/day45_Abstraction/shape/Shape.java
@@ -0,0 +1,26 @@
+package day45_Abstraction.shape;
+
+public abstract class Shape {
+    private final String name;
+
+    public Shape(String name) {
+        this.name = name;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public abstract double area();
+
+    public abstract double perimeter();
+
+    @Override
+    public String toString() {
+        return "Shape{" +
+                "name='" + getName() + '\'' +
+                "area='" + area() + '\'' +
+                "perimeter='" + perimeter() + '\'' +
+                '}';
+    }
+}
