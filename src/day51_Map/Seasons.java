511df5da2a79595ff2a97606e975501cf1701bac
Zeynep Sena <zeynepssaltik@gmail.com>
2022-05-26 14:38:48 +0300

    day51_Map

---
+++ src/day51_Map/Seasons.java
@@ -0,0 +1,8 @@
+package day51_Map;
+
+public enum Seasons {
+    WINTER,
+    SPRING,
+    SUMMER,
+    AUTMN
+}
--- src/day51_Map/Test_Enum.java
+++ src/day51_Map/Test_Enum.java
@@ -1,5 +1,38 @@
 package day51_Map;
 
 public class Test_Enum {
+    public static void main(String[] args) {
+// String  browser"wooden spoon";
+        Browser browser= Browser.SAFARI;
+
+        switch(browser){
+            case FIREFOX:
+                System.out.println("Firefox is selected");
+                break;
+
+            case SAFARI:
+                System.out.println("Safari is selected");
+                break;
+
+            case OPERA:
+                System.out.println("Opera is selected");
+                break;
+
+            case EDGE:
+                System.out.println("Edge is selected");
+                break;
+
+            default:
+                System.out.println("Chrome is selected");
+        }
+
+
+        System.out.println("-------------------");
+
+        Seasons season = Seasons.SPRING;
+
+
+    }
+
 
 }
